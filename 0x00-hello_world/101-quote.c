54efaef59eee56c1c60c603ce9867127795fca82
ZayaES <heavenphrince@gmail.com>
2022-10-13 14:41:48 -0700

    6

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
