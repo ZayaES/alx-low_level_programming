52dc3a5b638a29feb0e40ff7f51d629e5c6fa761
ZayaES <heavenphrince@gmail.com>
2022-10-15 05:02:20 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
