6711b09704b0deacc6bfa0c74fa55a245b6282cc
ZayaES <heavenphrince@gmail.com>
2022-10-15 04:22:14 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if ((n != 'e') || (n != 'q'))
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
