6112844514808e68c5d68db678c5a19de1c7d951
ZayaES <heavenphrince@gmail.com>
2022-10-15 04:58:34 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
