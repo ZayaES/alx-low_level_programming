9a2b78745cd8c82ec4bfbf026411e1091077aa62
ZayaES <heavenphrince@gmail.com>
2022-10-18 08:44:57 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "main.h"
+/* #include "main.h" */
 /**
  * print_alphabet - Function to write lowercase alphabet
  *
@@ -11,7 +11,7 @@ void print_alphabet(void)
 
 	for (n = 'a'; n <= 'z'; n++)
 	{
-		putchar(n);
+		_putchar(n);
 	}
 	_putchar('\n');
 }
