a465929808beb3b47ebfc74a1c4b60b18d8cb05b
ZayaES <heavenphrince@gmail.com>
2022-10-18 15:36:02 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+/* #include "main.h" */
+
+/**
+ * _abs - Checks if input is posirive, zero or negative
+ * @n: First operand
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+*/
+
+int _abs(int n)
+{
+#include "main.h"
+
+	if(n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1*n);
+	}
+}
