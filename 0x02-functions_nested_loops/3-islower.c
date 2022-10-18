a0eaea32cfa69c5dba1e9baf937d87ce478767c2
ZayaES <heavenphrince@gmail.com>
2022-10-18 14:56:00 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+/* #include "main.h" */
+
+/**
+ * _islower - Checks if input is lowercase
+ *
+ * Return: 1 if lowercase, 0 else
+*/
+
+int _islower(int c)
+{
+#include "main.h"
+{
+	if ((c >= 97) && (c <= 122))
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	};
+}
