012b9d56bd865fa8c55ba2992d95d1e02f358e45
ZayaES <heavenphrince@gmail.com>
2022-10-19 14:16:26 -0700

    ?

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,3 +15,22 @@ void print_to_98(int n)
 		_putchar(n);
 	}
 }
+
+/**
+ * print - print numbers
+ * @n: First operand
+ *
+ * Return: 1 if lowercase, 0 else
+ */
+
+void print(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n/10)
+		print(n/10);
+	_putchar(n%10 + '0');
+}	
