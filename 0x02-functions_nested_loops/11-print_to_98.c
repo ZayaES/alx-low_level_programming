e73bf3d14c2168ecd5e5987787ea552cc3fe246c
ZayaES <heavenphrince@gmail.com>
2022-10-19 14:32:07 -0700

    ?

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,19 @@ void print_to_98(int n)
 {
 #include "main.h"
 
-	for (; n <= 98; n++)
+	if (n <= 98)
 	{
-		print(n);
+		for (; n <= 98; n++)
+		{
+			print(n);
+		}
+	}
+	else if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			print(n);
+		}
 	}
 }
 
