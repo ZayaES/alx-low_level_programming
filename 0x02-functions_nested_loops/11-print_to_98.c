b81218b0d51feebaac57e970084db2064fd266a0
ZayaES <heavenphrince@gmail.com>
2022-10-19 13:45:31 -0700

    ?

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+/* #include "main.h" */
+/**
+ * print_to_98 - add 2 numbers
+ * @n: First operand
+ *
+ * Return: 1 if lowercase, 0 else
+ */
+
+void print_to_98(int n)
+{
+#include "main.h"
+
+	for (; n <= 98; n++)
+	{
+		_putchar(n);
+	}
+}
