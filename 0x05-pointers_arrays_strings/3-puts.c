3f539e35c923bdb7fea87378fa613f5c0655ca03
ZayaES <heavenphrince@gmail.com>
2022-10-24 13:02:51 -0700

    67

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: character
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar('*str + i');
+	}
+}
