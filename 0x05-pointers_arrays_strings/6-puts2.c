ef78d1d0141ceead4c91e88001922c7136c54e30
ZayaES <heavenphrince@gmail.com>
2022-10-25 03:04:15 -0700

    67

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other string
+ * @str: input string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		if ((j % 2) == 0)
+		{
+			_putchar(*(str + j));
+		}
+	}	
+}
