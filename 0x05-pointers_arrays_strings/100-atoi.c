5e52dca5db889cc0f305c1424b2faafcd5107279
ZayaES <heavenphrince@gmail.com>
2022-10-25 10:50:19 -0700

    67

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,8 +11,8 @@ int _atoi(char *s)
 	int i;
 	int j;
 	int k;
-//	int u = 0;
-	int *a;
+	int u = 0;
+	int *a = &u;
 
 	while (*(s + i) != '\0')
 	{
@@ -21,7 +21,7 @@ int _atoi(char *s)
 	k = i;
 	for (j = 0; j < k - 1; j++)
 	{
-		*a = *a + (*(s + i));// * (10 ** j);
+		*a = *a + (*(s + i));/* * (10 ** j);*/
 		i--;
 	}
 	return (i);
