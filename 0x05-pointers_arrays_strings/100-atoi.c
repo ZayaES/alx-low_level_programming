02ad0efea670ba7eca510501d3c24494b9e8d96c
ZayaES <heavenphrince@gmail.com>
2022-10-25 05:20:11 -0700

    67

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _atoi - covert strings to int
+ * @s: input string
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int u;
+	int *a = &u;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i - 1; j++)
+	{
+		*(a + j) = (*(s + j) - 0x30);
+	}
+	return (u);
+}
