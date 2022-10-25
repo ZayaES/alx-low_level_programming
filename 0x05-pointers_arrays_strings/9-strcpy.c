55c14c09057e1efd99981637215a1bbe6cc19e3b
ZayaES <heavenphrince@gmail.com>
2022-10-25 04:36:46 -0700

    67

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies from src to dest
+ * @dest: destination pointer
+ * @src: source pointer
+ */
+
+char _strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++
+	}
+	*(dest + i) = '\0';
+	return (*dest);
+}
