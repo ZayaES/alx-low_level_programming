e46a1d51f35d9e21fe55c5ec8432aab325924044
ZayaES <heavenphrince@gmail.com>
2022-10-31 09:57:17 -0700

    hello

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - finds the first occurrence of a character
+ * @s: string buffer
+ * @c: character to find
+ * Return: ptr
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char ptr;
+
+	i = 0;
+	while (*(s + i) != c)
+		i++;
+	ptr = s + i;
+
+	return (ptr);
+}
