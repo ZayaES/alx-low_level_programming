e1c2942e06d746d72c500719faca833434b23f2f
ZayaES <heavenphrince@gmail.com>
2022-10-31 09:13:43 -0700

    ga

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,4 +2,19 @@
 
 /**
  * _memcpy - copy memory from src to dest
- * @dest: 
+ * @dest: copied to
+ * src: copied from
+ * @n: place value
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
