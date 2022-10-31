fe7b0bd298dc25ea0cd9a614b32f6ebb77b61af0
ZayaES <heavenphrince@gmail.com>
2022-10-31 08:52:26 -0700

    ga

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - reset memory upto nth place
+ * @s:buffer to be reset
+ * @b: character that'd act as reseter
+ * @n: place to reset to
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned long int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
