e4a57c4e97c750ffe2bd116a1d2063973959cbe9
ZayaES <heavenphrince@gmail.com>
2022-11-05 06:04:36 -0700

    ty

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,13 @@ void _puts_recursion(char *s)
 	/*int i;*/
 
 	/* i = 0;*/
-	if ((*(s)) == '\0')
-		return;
+	if ((*(s)) != '\0')
+	{
 	_putchar(*(s));
 	/* i++;*/
 	s++;
 	_puts_recursion(s);
+	}
 	_putchar('s');
+	return;
 }
