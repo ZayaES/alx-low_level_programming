95d08cc20ab21fe65d43e203d9d091a90134cda9
ZayaES <heavenphrince@gmail.com>
2022-11-05 05:03:26 -0700

    567

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (*(s + i) = '\0')
+		return;
+	_putchar(*(s + i));
+	i++;
+	_puts_recursion(s + i);
+	_putchar('\0');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+prijects on recursion
