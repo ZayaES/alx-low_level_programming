bdef2c8e55695a1e81e6589a02815511f065b422
ZayaES <heavenphrince@gmail.com>
2022-11-07 07:52:13 -0800

    test of task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the arguments to the function
+ * @argc: counts number of argument
+ * @argv: points to the argument strings
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+
+	int x = 0;
+
+	while (i < argc)
+	{
+			if (atoi(argv[i]) >= 0)
+		{
+			x = x + atoi(argv[i]);
+			i++;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	if (argc >= 1)
+	{
+		printf("%d\n", x);
+		return (0);
+	}
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
+}
