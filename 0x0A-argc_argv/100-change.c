ab1d445a7011ba7f52c465ee4f4db46ca38471a2
ZayaES <heavenphrince@gmail.com>
2022-11-07 15:18:37 -0800

    2nd test for task5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - finds minimem count of coin 
+ * @argc: argument count
+ * @argv: vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int cent [5] = {25, 10, 5, 2, 1};
+	int i;
+	int change;
+	int n;
+
+	change = atoi(argv [1]);
+	n = 0;
+	i = 0;
+
+	if (argc == 2)
+	{
+		if (change > 0)
+		{
+			while (change != 0)
+			{
+				n = n + change / cent[i];		
+				change = change % cent[i];
+				i++;
+			}
+			printf("%d\n", n);
+		}
+		else
+		{
+			printf("0\n");
+		}
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
