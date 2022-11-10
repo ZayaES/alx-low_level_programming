684200e3087f6cd0361be59c353c3620c74b9688
ZayaES <heavenphrince@gmail.com>
2022-11-10 09:30:43 -0800

    1st test for task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,9 +10,9 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned long int i;
-	char *ptr;
-	char array[98];
+	char *array;
 
+	array = malloc(sizeof(char) * size);
 	if (size != 0)
 	{
 		for (i = 0; i < size; i++)
@@ -20,8 +20,7 @@ char *create_array(unsigned int size, char c)
 			array[i] = c;
 			i++;
 		}
-		ptr = array;
-		return (ptr);
+		return (array);
 	}
 	else
 		return NULL;
