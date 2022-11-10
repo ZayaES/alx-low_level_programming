00241b8159e9bf026bca00d2d75adb847763a84c
ZayaES <heavenphrince@gmail.com>
2022-11-10 11:34:50 -0800

    1st test of task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a copy of str
+ * @str: input string
+ * Return: ptr
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *ptr;
+
+	ptr = malloc(sizeof(*str));
+	if ((ptr == NULL || *str == NULL))
+	{
+		return (NULL);
+	}
+	i = 0;
+	while ((*str + i) != '\0')
+	{
+		*(ptr + i) = *(str + i);
+		i++;
+	}
+	return (ptr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
