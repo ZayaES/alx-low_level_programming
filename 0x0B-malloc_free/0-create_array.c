cc5a763c9d13a89ba75666ad28cf25dd71b5a432
ZayaES <heavenphrince@gmail.com>
2022-11-10 09:04:43 -0800

    1st test for task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * create_array - creates and initializes an array
+ * @size: number of elements in array
+ * @c: initializing char
+ * Return: char pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *ptr;
+	char array[size];
+
+	if (size != 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			array[i] = c;
+			i++;
+		}
+		ptr = array[];
+		return (ptr);
+	}
+	else
+		return;
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,3 @@
+int _putchar(char c);
+void reset_to_98(int *n);
+char *create_array(unsigned int size, char c);
