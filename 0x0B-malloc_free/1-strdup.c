0bf6b2a3547ada42396ba2c377f7bd5758f5f6e6
ZayaES <heavenphrince@gmail.com>
2022-11-10 12:14:05 -0800

    1st test of task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,7 +13,7 @@ char *_strdup(char *str)
 	int i;
 	char *ptr;
 
-	ptr = malloc(sizeof(str));
+	ptr = malloc(sizeof(str) * strlen (str));
 	if ((ptr == NULL) || (str == NULL))
 	{
 		return (NULL);
