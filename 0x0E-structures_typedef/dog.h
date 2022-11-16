9d747515fba96cc25b75d7408e4df590c7598957
ZayaES <heavenphrince@gmail.com>
2022-11-16 12:56:05 -0800

    1st test og task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog attributes
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
