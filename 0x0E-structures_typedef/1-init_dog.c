de199869f4570571decd0f007c9a70f023a84734
ZayaES <heavenphrince@gmail.com>
2022-11-16 13:51:48 -0800

    2nd test og task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,6 +10,8 @@
  * @owner: name of owner
  */
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {	
 	d = malloc(sizeof(struct dog));
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-void init_dog(struct dog *d, char *name, float age, char *owner);
 /**
  * struct dog - dog attributes
  * @name: name of dog
@@ -14,3 +13,5 @@ struct dog
 	char *owner;
 };
 #endif
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
