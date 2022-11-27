2bb9f2683d62f95db8504c95210ec925a9bcba09
ZayaES <heavenphrince@gmail.com>
2022-11-27 04:08:43 -0800

    2nd test of task0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,12 +5,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct list_s - used as a node of a list
+ * @str: string
+ * @len: length of string
+ * @next: pointer to next node
+ */
 
 typedef struct list_s
 {
-	    char *str;
-	        unsigned int len;
-		    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
