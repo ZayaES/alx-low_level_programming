4e2fb83c6a997a0f36e907f8ffb89a2ea0e5cde6
ZayaES <heavenphrince@gmail.com>
2022-11-27 03:59:42 -0800

    2nd test of task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,14 @@ size_t print_list(const list_t *h)
 	int i = 0;
 	while (h != NULL)
 	{
-		printf("[%ud] %s\n", h->len, h->str);
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		i++;
 	}
