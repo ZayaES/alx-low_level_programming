4338deb1124a15705802fcfc24999b8817ebd4f1
ZayaES <heavenphrince@gmail.com>
2022-11-29 06:56:29 -0800

    2nd test of task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * pop_listint - frees the head of a list
+ * @head: head to be freed
+ * Return: int data of head
+ */
+
+int pop_listint(listint_t **head)
+{
+	int *data = &((*head)->n);
+	listint_t *nextnode;
+
+	nextnode = (*head)->next;
+	free(*head);
+	*head = nextnode;
+
+	return (*data);
+}
