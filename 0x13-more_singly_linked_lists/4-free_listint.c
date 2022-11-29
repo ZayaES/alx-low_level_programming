26ad40f3a0c70f37c2151e7011f3bcb4ef99b846
ZayaES <heavenphrince@gmail.com>
2022-11-29 06:01:32 -0800

    1nth commit of task4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees entire allocation of a list
+ * @head: head of list input
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *lastnode; 
+	listint_t *next;
+
+
+	lastnode = head;
+	while (lastnode != NULL)
+	{
+		next = lastnode->next;
+		free(lastnode);
+		lastnode = next;
+	}
+}
