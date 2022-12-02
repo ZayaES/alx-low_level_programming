f7a012460786c6ccb79fd875f8a70b477a295f9c
ZayaES <heavenphrince@gmail.com>
2022-12-02 13:56:33 -0800

    1nth test of task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,9 +15,11 @@ unsigned int binary_to_uint(const char *b)
 
 	i = 0;
 	u = 0;
+	if (b == NULL)
+		return (0);
 	while (*(b + i) != '\0')
 	{
-		if ((*(b + i) > '1') || b == NULL)
+		if ((*(b + i) > '1'))
 			return (0);
 		i++;
 	}
