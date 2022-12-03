25f0613483d53e7b67dcf3019e3ff2b832e616a8
ZayaES <heavenphrince@gmail.com>
2022-12-02 16:01:43 -0800

    1nth test of task2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_int - checks bit in a place specified  by index
+ * get_bit - checks bit in a place specified  by index
  * @n: input integer
  * @index: nth place to check bit
  * Return: bit in nth place
@@ -12,6 +12,8 @@ int get_bit(unsigned long int n, unsigned int index)
 	int u;
 
 	u = ((n & 0xFFFFFFFFFFFFFFFF) >> index) & 1;
+	if (index > 64)
+		return (-1);
 
 	return (u);
 }
