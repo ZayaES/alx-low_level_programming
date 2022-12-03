f2fa5898b69e0abf6114262d663f1e5ca2a7149d
ZayaES <heavenphrince@gmail.com>
2022-12-02 17:11:03 -0800

    1nth test of task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bit required to be flipped to move from one number to another
+ * @n: initial number
+ * @m: final number
+ * Return: the number of bits requkred
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int l;
+	unsigned int i, count;
+
+	l = n ^ m;
+	i = 0;
+	count = 0;
+
+	while (i < 64)
+	{
+		count = count + (l & 1);
+		l = l >> 1;
+		i++;
+	}
+	return (count);
+}
