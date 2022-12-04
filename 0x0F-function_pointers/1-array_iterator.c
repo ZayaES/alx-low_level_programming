ef9e67444dd2d4886c5c7b661d7bd195b13192da
ZayaES <heavenphrince@gmail.com>
2022-12-04 01:06:44 -0800

    1nth test of task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through array
+ * @array: input array
+ * @size: size of array
+ * @action: specifies what should be done with array
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(*(array + i));
+	}
+}
