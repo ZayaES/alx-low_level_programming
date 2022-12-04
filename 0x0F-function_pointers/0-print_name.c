35ee1243f94d96b4bd6e4b54c06423a127748670
ZayaES <heavenphrince@gmail.com>
2022-12-04 00:32:57 -0800

    1st test of task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,13 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - prints passed strings
+ * @name: passed string
+ * @f: function used to print string
+ * function f determines how the name will be printed
+ * Return: void
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
