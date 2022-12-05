4edc10b4c094baf96a6499105ddd40eaed92bbca
ZayaES <heavenphrince@gmail.com>
2022-12-05 11:37:05 -0800

    1nth test of task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		close(fd);
 		return (-1);
 	}
 	wr = write(fd, text_content, strlen(text_content));
