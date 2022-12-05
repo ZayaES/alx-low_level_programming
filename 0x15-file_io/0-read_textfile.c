c9b1147b9dad9a9f44f4b218b2bcaf4d6c197c3c
ZayaES <heavenphrince@gmail.com>
2022-12-05 11:24:00 -0800

    1nth test of task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,9 +21,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	rd = read(fd, buf, letters);
 	if (rd == -1)
+	{
 		close (fd);
 		return (0);
-
+	}
 	close(fd);
 
 	count = write(1, buf, rd);
