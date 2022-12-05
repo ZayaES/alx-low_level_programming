aa8958c76971ee3014fba6d1801adcb8bba0f1f1
ZayaES <heavenphrince@gmail.com>
2022-12-05 14:14:36 -0800

    1nth test of task3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ */
+
+int main (int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit (97);
+	}
+	cp (argv[1], argv[2]);
+
+	return (0);
+}
+
+void cp (char *file_from, char *file_to)
+{
+	int fd, fd2, cl, cl2;
+	ssize_t rd, wr;
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+	fd = open(file_from, O_RDWR);
+	rd = read(fd, buf, 1024);
+	if (rd < 0)
+	{
+		dprintf(2, "Error: Can't read from %s\n", file_from);
+		exit(98);
+	}
+	fd2 = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	wr = write(fd2, buf, rd);
+	if (wr < 0)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file_from);
+		exit(99);
+	}
+	cl = close(fd);
+	if (cl == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	cl2 = close(fd2);
+	if (cl2 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+}
+
